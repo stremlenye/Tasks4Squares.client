e730505185d252a981b7b11b18722dd2a51d4ed8
stremlenye <stremlenye@gmail.com>
2015-07-13 00:03:01 +0200

    cleanup

--- app/root.js
+++ app/root.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Redirect, Router, Route, DefaultRoute } from 'react-router'
-import HashHistory from 'react-router/lib/HashHistory';
+import { Redirect, Router, Route } from 'react-router'
+import HashHistory from 'react-router/lib/HashHistory'
 import { Provider } from 'redux/react'
 import { createStore } from 'redux'
 import { Application, Signin, Tasks } from 'components'
@@ -9,7 +9,6 @@ import * as reducers from 'reducers'
 const store = createStore(reducers)
 
 function renderRoutes () {
-  console.log('ROUTES')
   return (
     <Router history={new HashHistory()}>
       <Route path="/" component={Application}>
@@ -23,7 +22,6 @@ function renderRoutes () {
 
 class Root extends React.Component {
   render () {
-    console.log('ROOT')
     return (
       <Provider store={store}>
         {() => {return renderRoutes()}}
