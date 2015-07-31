b6b66dcfa6a79508202b8a82d62feb8ec2b40622
stremlenye <stremlenye@gmail.com>
2015-07-31 11:58:49 +0200

    withBody -> body

--- app/actions/user.js
+++ app/actions/user.js
@@ -3,12 +3,12 @@ import { setToken, deleteToken } from 'persistence'
 import { LOGGED_IN, SIGNED_UP, LOGGED_OUT } from 'constants/user'
 
 export function signup (name, login, password) {
-  return dispatch => api.signup.withBody({name, login, password }).exec()
+  return dispatch => api.signup.body({name, login, password }).exec()
     .then(() => dispatch({ type: SIGNED_UP }))
 }
 
 export function signin (login, password) {
-  return dispatch => api.signin.withBody({ login, password }).exec()
+  return dispatch => api.signin.body({ login, password }).exec()
     .then(({ response: { token }}) => {
       setToken(token)
       return dispatch({ type: LOGGED_IN })
